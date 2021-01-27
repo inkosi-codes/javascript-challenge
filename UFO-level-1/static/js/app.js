2204310b82a16345b79d20a0245e97ee922795eb
unknown <kiwwanne@gmail.com>
2021-01-27 00:52:49 -0600

    date formatting issue

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -8,34 +8,69 @@ window.onload = function hide() {
 
 // If user needs to use advanced search functions once check box is selected display html elements
 function adv() {
-   var chk = document.getElementById("multi-search");
+    var chk = document.getElementById("multi-search");
 
-   if(chk.checked == true){
-    document.getElementById('state-label').style.display = 'block';
-    document.getElementById('city-label').style.display = 'block';
-   }
-   else{
-    document.getElementById('state-label').style.display = 'none';
-    document.getElementById('city-label').style.display = 'none';
-   };
+    if (chk.checked == true) {
+        document.getElementById('state-label').style.display = 'block';
+        document.getElementById('city-label').style.display = 'block';
+    }
+    else {
+        document.getElementById('state-label').style.display = 'none';
+        document.getElementById('city-label').style.display = 'none';
+    };
 };
 
 
 // Main JS for creating and loading html table onto the page
+var tableData = data;
 var tbody = d3.select("tbody");
 
-
-var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"] //used to access columns inside of the data
-var columnUpper = ["city", "state", "country", "shape"] // Created in order to manipulate certain columns by turing them into uppercase
+var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]; //used to access columns inside of the data
+var columnUpper = ["city", "state", "country", "shape"]; // Created in order to manipulate certain columns by turing them into uppercase
 
 //Inputing the data into the HTML
-data.forEach(Sighting => {
-    var row = tbody.append("tr");
-    columns.forEach(column =>
-        row.append("td").text(
-            (columnUpper.includes(column)) ?
-                Sighting[column].toUpperCase() : Sighting[column])
-    )
+var ufoData = (getData) => {
+    getData.forEach(Sighting => {
+        var row = tbody.append("tr");
+        columns.forEach(column =>
+            row.append("td").text(
+                (columnUpper.includes(column)) ?
+                    Sighting[column].toUpperCase() : Sighting[column])
+        )
+    });
+};
+ufoData(tableData);
+
+
+var filter = d3.select("#filter-btn");
+
+filter.on("click", function() {
+    tbody.html("");
+
+    d3.event.preventDefault();
+    var inputElement = d3.select("#datetime");
+    var inputValue = inputElement.property("value");
+
+    var date = new Date(inputValue);
+
+    var inputValue = date.toLocaleDateString('en-US', {timeZone: 'UTC'});
+
+    var filterDate = tableData.filter(tableData => tableData.datetime === inputValue);
+
+    let response = {
+        filterDate
+    }
+
+
+    if (response.filterDate.length !== 0) {
+        ufoData(filterDate);
+    }
+
+    // Top if only works for filtering the date
+
+    else {
+        
+    }
 });
 
 // Used to populate the dropdown box with unique values for state abbrv
